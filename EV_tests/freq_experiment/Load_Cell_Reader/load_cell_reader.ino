802a6776ae1acc336778fd4293caaa73f03cadb5
federicofavotto <federico.favotto94@gmail.com>
2021-12-02 15:28:44 +0100

    upload test frequenze

---
+++ EV_tests/freq_experiment/Load_Cell_Reader/load_cell_reader.ino
@@ -0,0 +1,109 @@
+/*
+   -------------------------------------------------------------------------------------
+   HX711_ADC
+   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
+   Olav Kallhovd sept2017
+   -------------------------------------------------------------------------------------
+*/
+/*
+   Settling time (number of samples) and data filtering can be adjusted in the config.h file
+   For calibration and storing the calibration value in eeprom, see example file "Calibration.ino"
+   The update() function checks for new data and starts the next conversion. In order to acheive maximum effective
+   sample rate, update() should be called at least as often as the HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS.
+   This example shows how call the update() function from an ISR with interrupt on the dout pin.
+   Try this if you experince longer settling time due to time consuming code in the loop(),
+   i.e. if you are refreshing an graphical LCD, etc.
+   The pin used for dout must be external interrupt capable.
+*/
+
+/**
+ * @file load_cell_reader.ino
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-12-01
+ *
+ * @copyright Copyright (c) 2021
+ *
+ *
+ * PINMAP arduino mega
+ * https://www.arduino.cc/en/Hacking/PinMapping2560
+ */
+#define PWM0 2
+#define PWM1 3
+#define PWM2 4
+#define PWM_MASK 0x0C
+#define PWM_SHIFT 1
+
+#define EXPERIMENT_CONTROL
+//#define TARE_CONTROL
+
+
+#define PIN_EXPERIMENT 4
+
+#include <HX711_ADC.h>
+
+const int HX711_dout = 6; // mcu > HX711 dout pin, must be external interrupt capable!
+const int HX711_sck = 7;  // mcu > HX711 sck pin
+
+// HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+unsigned long t = 0;
+boolean newDataReady;
+
+void setup()
+{
+  pinMode(PWM0, INPUT);
+  pinMode(PWM1, INPUT);
+  pinMode(PWM2, INPUT);
+  pinMode(PIN_EXPERIMENT, INPUT);
+  Serial.begin(57600);
+  delay(10);
+  Serial.println();
+  Serial.println("Starting...");
+
+  float calibrationValue; // calibration value
+  calibrationValue = 1.0; // uncomment this if you want to set this value in the sketch
+  LoadCell.begin();
+  // LoadCell.setReverseOutput();
+  unsigned long stabilizingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilizing time
+  boolean _tare = false;                // set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag())
+  {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1)
+      ;
+  }
+  else
+  {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
+}
+
+void loop()
+{
+  const int serialPrintInterval = 10; // increase value to slow down serial print activity
+
+#ifdef EXPERIMENT_CONTROL
+  byte inExperiment = digitalRead(PIN_EXPERIMENT);
+#else
+  byte inExperiment = 1;
+#endif
+  if (LoadCell.update())
+    newDataReady = true;
+  // get smoothed value from the dataset:
+  if (newDataReady && inExperiment)
+  {
+    if (millis() > t + serialPrintInterval)
+    {
+      float i = LoadCell.getData();
+      // Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+}
---
+++ EV_tests/freq_experiment/PWM_generator/pwm_gen.ino
@@ -0,0 +1,73 @@
+//#define HIGH_FREQUENCY
+//#define DEBUG_LOG
+
+#define PIN_OUTPUT 3
+#define PIN_EXPERIMENT 6
+
+#ifdef HIGH_FREQUENCY
+#define DELAY 10000
+#define PWM_VALUE 128
+#endif
+
+#ifndef HIGH_FREQUENCY
+#define PWM_DELAY_STEP_MS 16 // 8 ms delay step = 8*2^bits ms period
+#define PWM_MAX 7
+#define PWM_SET 1 // 0 to 7 setpoints
+
+#define LOOP_COUNT 50
+#define INITIAL_DELAY 10000
+#endif
+#include <Arduino.h>
+
+void setup()
+{
+#if defined(HIGH_FREQUENCY)
+    // TCCR2B = (TCCR2B & 0b11111000) | 0x05;   // 245.10 [Hz]
+    // TCCR2B = (TCCR2B & 0b11111000) | 0x06;   // 122.55 [Hz]
+    TCCR2B = (TCCR2B & 0b11111000) | 0x07; // 30.64 [Hz]
+#endif
+    pinMode(PIN_OUTPUT, OUTPUT);
+    pinMode(PIN_EXPERIMENT, OUTPUT);
+
+    delay(INITIAL_DELAY);
+    digitalWrite(PIN_EXPERIMENT, HIGH);
+    for (size_t cycle = 0; cycle < LOOP_COUNT; cycle++)
+    {
+
+        if (PWM_SET > 0)
+            digitalWrite(PIN_OUTPUT, HIGH);
+        for (size_t i = 0; i < PWM_MAX; i++)
+        {
+            if (i == PWM_SET)
+            {
+                digitalWrite(PIN_OUTPUT, LOW);
+            }
+            delay(PWM_DELAY_STEP_MS);
+        }
+        /* code */
+    }
+    digitalWrite(PIN_EXPERIMENT, LOW);
+    digitalWrite(PIN_OUTPUT, LOW);
+}
+
+void loop()
+{
+#if !defined(HIGH_FREQUENCY)
+    // if (PWM_SET > 0)
+    //         digitalWrite(PIN_OUTPUT, HIGH);
+    //     for (size_t i = 0; i < PWM_MAX; i++)
+    //     {
+    //         if (i == PWM_SET)
+    //         {
+    //             digitalWrite(PIN_OUTPUT, LOW);
+    //         }
+    //         delay(PWM_DELAY_STEP_MS);
+    //     }
+
+#else
+    analogWrite(PIN_OUTPUT, PWM_VALUE); // half opened
+    delay(DELAY);
+    analogWrite(PIN_OUTPUT, 0); // half opened
+    delay(DELAY);
+#endif // HIGH_FREQUENCY
+}
