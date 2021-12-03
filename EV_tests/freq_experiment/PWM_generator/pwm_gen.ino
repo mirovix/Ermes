6bebef3dc51d903932a9ce2da0ef365eccc4510f
federicofavotto <federico.favotto94@gmail.com>
2021-12-03 15:22:36 +0100

    dati 30 Hz

--- EV_tests/freq_experiment/PWM_generator/pwm_gen.ino
+++ EV_tests/freq_experiment/PWM_generator/pwm_gen.ino
@@ -1,33 +1,68 @@
-//#define HIGH_FREQUENCY
+#define HIGH_FREQUENCY
 //#define DEBUG_LOG
+#include <Arduino.h>
 #define PRINT_PWM
+
+#ifdef PRINT_PWM
+#define PWM0 4
+#define PWM1 5
+#define PWM2 6
+#define PWM3 7
+#define PWM_MASK 0xF0
+#define PWM_SHIFT 4
+byte portd_supp;
+#define writePWMData(pwm) \
+portd_supp = PORTD; \
+portd_supp &= ~PWM_MASK; \
+PORTD = portd_supp | ((pwm << PWM_SHIFT) & PWM_MASK);
+#endif
 
 #define PIN_OUTPUT 3
 #define PIN_EXPERIMENT 2
 
 #ifdef HIGH_FREQUENCY
-#define DELAY 1000
-#define PWM_VALUE 128
+#define DELAY 500
+byte pwm_real;
 #else
-#define PWM_DELAY_STEP_MS 6 // 8 ms delay step = 8*2^bits ms period
+#define PWM_DELAY_STEP_MS 32 // 8 ms delay step = 8*2^bits ms period
 #define PWM_MAX 7
-#define PWM_SET 5 // 0 to 7 setpoints
+#define PWM_SET 1 // 0 to 7 setpoints
 #define LOOP_COUNT 100
-#define INITIAL_DELAY 10000
 #endif
-#include <Arduino.h>
+#define INITIAL_DELAY 10000
 
 void setup()
 {
+#ifdef PRINT_PWM
+    pinMode(PWM0, OUTPUT);
+    pinMode(PWM1, OUTPUT);
+    pinMode(PWM2, OUTPUT);
+#ifdef PWM3
+    pinMode(PWM3, OUTPUT);
+#endif
+#endif
     pinMode(PIN_OUTPUT, OUTPUT);
     pinMode(PIN_EXPERIMENT, OUTPUT);
+
     delay(INITIAL_DELAY);
 
-#if defined(HIGH_FREQUENCY)
-    TCCR2B = (TCCR2B & 0b11111000) | 0x05; // 245.10 [Hz]
-    // TCCR2B = (TCCR2B & 0b11111000) | 0x06;   // 122.55 [Hz]
-    // TCCR2B = (TCCR2B & 0b11111000) | 0x07; // 30.64 [Hz]
-#else
+#if defined(HIGH_FREQUENCY) //HIGH FREQUENCY EXPERIMENT SECTION
+    //TCCR2B = (TCCR2B & 0b11111000) | 0x05; // 245.10 [Hz]
+    
+    //TCCR2B = (TCCR2B & 0b11111000) | 0x06;   // 122.55 [Hz]
+    TCCR2B = (TCCR2B & 0b11111000) | 0x07; // 30.64 [Hz]
+    digitalWrite(PIN_EXPERIMENT, HIGH);
+    for (size_t i = 0, pwm_real=0; pwm_real<256; ++i, pwm_real+=16)
+    {
+        writePWMData(i);
+        analogWrite(PIN_OUTPUT, pwm_real+15); 
+        delay(DELAY);
+    }
+    digitalWrite(PIN_EXPERIMENT, LOW);
+    analogWrite(PIN_OUTPUT, 0);
+#else // LOW FREQUENCY EXEPERIMENT SECTION
+
+    writePWMData(PWM_SET);
 
     digitalWrite(PIN_EXPERIMENT, HIGH);
     for (size_t cycle = 0; cycle < LOOP_COUNT; cycle++)
@@ -65,9 +100,9 @@ void loop()
     //     }
 
 #else
-    analogWrite(PIN_OUTPUT, PWM_VALUE); // half opened
-    delay(DELAY);
-    analogWrite(PIN_OUTPUT, 0); // half opened
-    delay(DELAY);
+    // analogWrite(PIN_OUTPUT, PWM_VALUE); // half opened
+    // delay(DELAY);
+    // analogWrite(PIN_OUTPUT, 0); // half opened
+    // delay(DELAY);
 #endif // HIGH_FREQUENCY
 }
