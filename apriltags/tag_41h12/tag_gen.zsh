6d5c543dfe2608e08ab62bd7b74bbed6293f630a
federicofavotto <federico.favotto94@gmail.com>
2022-01-21 12:54:36 +0100

    tetest 41h12

---
+++ apriltags/tag_41h12/tag_gen.zsh
@@ -0,0 +1,7 @@
+#!/bin/sh
+convert tag41_12_00000.png -scale 1000% tag41_12_00000_1000.png
+convert tag41_12_00001.png -scale 1250% tag41_12_00001_1250.png
+convert tag41_12_00002.png -scale 1500% tag41_12_00002_1500.png
+convert tag41_12_00003.png -scale 1750% tag41_12_00003_1750.png
+convert tag41_12_00004.png -scale 2000% tag41_12_00004_2000.png
+convert tag41_12_00005.png -scale 2500% tag41_12_00005_2500.png
