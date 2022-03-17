3e36a10ca02462c5921d2c9a28ca2cddf6f35ede
mirovix <65248348+mirovix@users.noreply.github.com>
2022-03-18 00:42:49 +0100

    imu_ros init

--- low_lvl/low_lvl_with_imu/low_lvl_with_imu.ino
+++ low_lvl/low_lvl_with_imu/low_lvl_with_imu.ino
@@ -1,3 +1,6 @@
+#include <Ixmatix_LSM9DS1.h>
+#include <ros.h>
+#include <sensor_msgs/Imu.h>
 
 //GLOBAL VARIABLES
 //[0-5] positive axis [6-11] negative axis
@@ -22,13 +25,85 @@ char *ev_config[2*dof];
 unsigned long start,loop_start = 0;
 
 const uint32_t INTERVAL_ROS_MSG = (uint32_t) 1.f/10.f*1000; // 10Hz
+unsigned long lastRefreshTime1 = 0, lastRefreshTime2 = 0;
+
+/*
+ * Ros variables
+ */
+ros::NodeHandle nh;
+sensor_msgs::Imu imu1_msg, imu2_msg;
+ros::Publisher imu1_pub("/chaser/sensors/imu1", &imu1_msg);
+ros::Publisher imu2_pub("/chaser/sensors/imu2", &imu2_msg);
+
+Ixmatix_LSM9DS1 imu1,imu2;
+
+// deltat variables
+int32_t lastUpdate1, lastUpdate2 = 0; 
+uint32_t now1 = 0, now2 = 0;
+
+/*
+ * Calibration variables
+ */
+
+// Magnetic Mapping
+// Soft iron error compensation matrix
+float mag_softiron_matrix[3][3] = { { +0.990, -0.002, -0.005 },
+                                    { -0.002, +0.989, -0.075 },
+                                    { -0.005, -0.075, +1.027 }
+                                  };
+
+// xl = accelerometer ----- g  = gyroscope
+float xl_offsets[3] = { +0.04f, -0.10f, -0.01f  };
+float g_offsets[3]  = { -0.05f, +0.35f, -0.73f  };
+
+// Magnetic calibration data - obtaied from Motion Sensor Calibration Tool
+// link - https://www.pjrc.com/store/prop_shield.html
+// Magnetic offset
+float mag_offsets[3]            = { -5.63/1000., 16.78/1000., -6.8/1000. };
+
+/*
+ * LSM9DS1 variables
+ */
+float aRes1, aRes2;
+float gRes1, gRes2;
+float mRes1, mRes2;
+
+float ax1, ay1, az1, ax2, ay2, az2;
+float gx1, gy1, gz1, gx2, gy2, gz2;
+float mx1, my1, mz1, mx2, my2, mz2;
+
+int16_t ax_1, ay_1, az_1, ax_2, ay_2, az_2;
+int16_t gx_1, gy_1, gz_1, gx_2, gy_2, gz_2;
+
+uint8_t statusXL_G1, statusXL_G2;
+uint8_t statusM1, statusM2;
+
+uint8_t newXLData1, newXLData2;
+uint8_t newGData1, newGData2;
+uint8_t newMData1, newMData2;
+
+int16_t sensorValues1[3], sensorValues2[3];
+
+char name_imu1[] = "imu1";
+char name_imu2[] = "imu2";
 
 
 void setup() {
 
   Serial.begin(230400);
-
+  nh.getHardware()->setBaud(230400);
+  nh.initNode();
+  nh.advertise(imu1_pub);
+  nh.advertise(imu2_pub);
   randomSeed(analogRead(0));
+
+  Wire.begin();
+
+  //setup imu1
+  imuSetup(imu1, aRes1, gRes1, mRes1);
+  //setup imu2
+  //imuSetup(imu2, aRes2, gRes2, mRes2);
+
 
   //leds
   pinMode(2, OUTPUT);
@@ -58,8 +133,7 @@ void setup() {
 }
 
 void loop() {
-  loop_start = millis();
-  if(!flag_thruster_open){
+  if(false){
     if(Serial.available()){
       flag_thruster_open = true;      
       length_input = Serial.readBytes(input_serial, 8);
@@ -70,7 +144,6 @@ void loop() {
         Serial.println(input_serial);
         
         cycles = (input_serial[4] - '0') * 100 + (input_serial[5] - '0') * 10 + input_serial[6] - '0';
-        cycles--;
 
         start = millis();
         OpenThrusters(input_serial);
@@ -79,18 +152,28 @@ void loop() {
       }
     }
   }
-  else
-    count_cycles++;
+  //else
+  //  count_cycles++;
   
-  //Serial.println(millis()-loop_start);  
-  
-  if(count_cycles > cycles and cycles != -1){
+  if(((millis() - start) > cycles*INTERVAL_ROS_MSG) and cycles != -1){
     CloseThrusters(); 
     Serial.print("Effective time:");
     Serial.println(millis()-start);
   }
+
+  //imus updating
+  imuUpdate(imu1, aRes1, gRes1, mRes1, statusXL_G1, statusM1, newXLData1, newGData1, newMData1, sensorValues1,
+  ax1, ay1, az1, gx1, gy1, gz1, mx1, my1, mz1, lastRefreshTime1, lastUpdate1, now1);
+  //imuUpdate(imu2, aRes2, gRes2, mRes2, statusXL_G2, statusM2, newXLData2, newGData2, newMData2, sensorValues2,
+  //ax2, ay2, az2, gx2, gy2, gz2, mx2, my2, mz2, lastRefreshTime2, lastUpdate2, now2); 
+
+  //delay(INTERVAL_ROS_MSG-(millis()-loop_start));
+  //check and publish the msgs
+  imuPublish(imu1, gRes1, mRes1, ax1, ay1, az1, gx1, gy1, gz1, imu1_msg, imu1_pub, lastRefreshTime1, name_imu1);
+  //imuPublish(imu2, gRes2, mRes2, ax2, ay2, az2, gx2, gy2, gz2, imu2_msg, imu2_pub, lastRefreshTime2, name_imu2);
+  //Serial.print("Effective time:");
+  //Serial.println(millis()-loop_start);
   
-  delay(INTERVAL_ROS_MSG-(millis()-loop_start));
 }
 
 void OpenThrusters(char *input){
@@ -137,4 +220,145 @@ void print_error(char *err)
   Serial.print(err);
   Serial.print("\n\n");
 	return;
+}
+
+void imuSetup(Ixmatix_LSM9DS1 &imu, float &aRes, float &gRes, float &mRes){
+  
+  // Wait for sensor
+  while (!imu.isLSM9DS1Ready());
+
+  imu.setConfig();
+
+  // Mahony variables
+  imu.Kp = (2.0f * 6.0f);
+  imu.Ki = (2.0f * 0.0f);
+  
+  // Madgwick variables
+  imu.updateBeta(15.f);
+  
+  imu.initLSM9DS1();                // Setting sensor registers to get data
+  imu.getRes(&aRes, &gRes, &mRes);  // Get resolution to convert raw data to real data
+}
+
+void imuUpdate(Ixmatix_LSM9DS1 &imu, float &aRes, float &gRes, float &mRes, 
+uint8_t &statusXL_G, uint8_t &statusM, 
+uint8_t &newXLData, uint8_t &newGData, uint8_t &newMData, int16_t *sensorValues,
+float &ax, float &ay, float &az, float &gx, float &gy, float &gz, float &mx, float &my, float &mz,
+unsigned long &lastRefreshTime, int32_t &lastUpdate, uint32_t &now){
+  
+  // Read accelerometer and gyroscope register status from sensor
+  imu.readI2C(LSM9DS1_ADDR_XG, LSM9DS1_R_XG_STATUS_REG, 1, &statusXL_G);
+  // Get a boolean that indicates if there's new data available from status register
+  newXLData = statusXL_G & 0x01;
+  newGData  = statusXL_G & 0x02;
+  // Read magnetometer register status from sensor
+  imu.readI2C(LSM9DS1_ADDR_M, LSM9DS1_R_M_STATUS_REG_M, 1, &statusM);
+  // Get a boolean that indicates if there's new data available from status register
+  newMData  = statusM & 0x08;
+
+  // is accelerometer data available?
+  if (newXLData) {
+    imu.readSensorData(LSM9DS1_ADDR_XG, LSM9DS1_R_XG_OUT_X_L_XL, sensorValues); // get sensor values
+    
+    // The data that is returned from the sensor is
+    // raw data obtained by the ADC (Analog to Digital Converter) as LSB units
+    // we need to multiply this value to its resolution converter
+    // to obtain real data. Check page 12 from datasheet
+    ax = (float)sensorValues[0] * aRes - xl_offsets[0]; // offset or bias is subtracted to get calibrated data
+    ay = (float)sensorValues[1] * aRes - xl_offsets[1];
+    az = (float)sensorValues[2] * aRes - xl_offsets[2];
+  }
+  
+  // is gyoscope data available?
+  if (newGData) {
+    imu.readSensorData(LSM9DS1_ADDR_XG, LSM9DS1_R_XG_OUT_X_L_G, sensorValues); // get sensor values
+    
+    gx = (float)sensorValues[0] * gRes - g_offsets[0];  // offset or bias is subtracted to get calibrated data
+    gy = (float)sensorValues[1] * gRes - g_offsets[1];
+    gz = (float)sensorValues[2] * gRes - g_offsets[2];
+  }
+  
+  // is accelerometer data available?
+  if (newMData) {
+    imu.readSensorData(LSM9DS1_ADDR_M, LSM9DS1_R_M_OUT_X_L_M, sensorValues); // get sensor values
+
+    // Compensating for Hard-Iron Effects
+    float mx_temp = (float)sensorValues[0] * mRes - mag_offsets[0];  // offset or bias is subtracted to get calibrated data
+    float my_temp = (float)sensorValues[1] * mRes - mag_offsets[1];
+    float mz_temp = (float)sensorValues[2] * mRes - mag_offsets[2];
+    
+    // Compensating for Soft-Iron Effects
+    mx = mx_temp * mag_softiron_matrix[0][0] + my_temp * mag_softiron_matrix[0][1] + mz_temp * mag_softiron_matrix[0][2];
+    my = mx_temp * mag_softiron_matrix[1][0] + my_temp * mag_softiron_matrix[1][1] + mz_temp * mag_softiron_matrix[1][2];
+    mz = mx_temp * mag_softiron_matrix[2][0] + my_temp * mag_softiron_matrix[2][1] + mz_temp * mag_softiron_matrix[2][2];
+  }
+
+  // Get dt expressed in sec for madgwick/mahony algorithms
+  now = micros();
+  imu.deltat = ((now - lastUpdate) / 1000000.0f);
+  lastUpdate = now;
+
+  // Algorithm filter
+  // mx is sent as the negative of mx due to axis coordinate difference
+  // check datasheet page 10
+  // y-axis is sent as the negative of y-axis to correct quaternion orientation
+  // Madgwick and Mahony algorithms require rad/seg for gyroscope values
+  // 1 rad/seg = 1 deg * (PI/ 180)
+  //                          (                 |                   |                   |   |    |   |    |    |   )
+  imu.updateMadgwickQuaternion(gx * PI / 180.0f , -gy * PI / 180.0f , gz * PI / 180.0f  , ax, -ay, az, -mx, -my, mz);
+  //imu.updateMahonyQuaternion(  gx*PI/180.0f     , -gy*PI/180.0f     , gz*PI/180.0f      , ax, -ay, az, -mx, -my, mz);
+}
+
+void imuPublish(Ixmatix_LSM9DS1 &imu, float &gRes, float &mRes, 
+float &ax, float &ay, float &az, float &gx, float &gy, float &gz,
+sensor_msgs::Imu &imu_msg, ros::Publisher &imu_pub,
+unsigned long &lastRefreshTime, char *name)
+{
+  if (millis() - lastRefreshTime >= INTERVAL_ROS_MSG){
+  
+    // sensor_msgs/Imu Message
+    // http://docs.ros.org/kinetic/api/sensor_msgs/html/msg/Imu.html
+    // you can define frame_id with a representative name for your robot
+    imu_msg.header.frame_id =  name;
+    imu_msg.header.stamp = nh.now();
+    imu_msg.orientation.x = imu.q[1];
+    imu_msg.orientation.y = imu.q[2];
+    imu_msg.orientation.z = imu.q[3];
+    imu_msg.orientation.w = imu.q[0];
+
+    // Imu message requires rad/sec for rotational velocity
+    // LSM9DS1 gyroscope sensor gives deg/seg
+    // 1 rad/seg = 1 deg * (PI/ 180)
+    imu_msg.angular_velocity.x = gx * PI / 180.0f;
+    imu_msg.angular_velocity.y = gy * PI / 180.0f;
+    imu_msg.angular_velocity.z = gz * PI / 180.0f;
+
+    // Imu message requires m/s^2 (not g's) for linear acceleration
+    // LSM9DS1 accelerometer sensor gives mg data
+    // 1 m/s^2 = 1 mg*(1g / 1000mg)*(9.81 m/s^2 / 1g) = 0.00981 m/s^2
+    imu_msg.linear_acceleration.x = ax * 0.00981;
+    imu_msg.linear_acceleration.y = ay * 0.00981;
+    imu_msg.linear_acceleration.z = az * 0.00981;
+    
+    // Gyroscope bias is the standard deviation
+    // variance is just the square of it.
+    imu_msg.angular_velocity_covariance[0] = g_offsets[0]*g_offsets[0];
+    imu_msg.angular_velocity_covariance[4] = g_offsets[1]*g_offsets[1];;
+    imu_msg.angular_velocity_covariance[8] = g_offsets[2]*g_offsets[2];
+    
+    // Accelerometer bias is the standard deviation
+    // variance is just the square of it
+    imu_msg.linear_acceleration_covariance[0] = xl_offsets[0]*xl_offsets[0];
+    imu_msg.linear_acceleration_covariance[4] = xl_offsets[1]*xl_offsets[1];
+    imu_msg.linear_acceleration_covariance[8] = xl_offsets[2]*xl_offsets[2];
+    
+    // orientation_covariance is filled with zeros to
+    // indicate a "covariance unknown"
+
+    // send imu_msg to ros
+    //imu_pub.publish(&imu_msg);
+    Serial.println(millis()-lastRefreshTime);
+    lastRefreshTime = millis();
+    nh.spinOnce();
+  }
 }
